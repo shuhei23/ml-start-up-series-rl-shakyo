b8fb97dca241af31a5a1a671d9c25ca8fceaeab5
shuhei23 <shuhei.toda@gmail.com>
2021-10-23 14:47:18 +0900

    v-for

--- DP/static/js/index.js
+++ DP/static/js/index.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el:'#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        users: [
+            {name: 'Adam'},
+            {name: 'Ben'},
+            {name: 'Chris'}
+        ]
     }
 })
