e09c703af85758c9db6628209512a82d4e2d6134
shuhei23 <shuhei.toda@gmail.com>
2021-12-29 20:33:51 +0900

    タイポを修正

--- FN/nn_tutorial/explanation_keras_batch.py
+++ FN/nn_tutorial/explanation_keras_batch.py
@@ -2,9 +2,9 @@ import numpy as np
 from tensorflow.python import keras as K
 
 model = K.Sequential([
-    K.layers.Dense(unit=4, input_shape=((2,)),# input_shape は入力の数(次元)
+    K.layers.Dense(units=4, input_shape=((2, )),# input_shape は入力の数(次元)
                     activation="sigmoid"),# unit は出力の数(次元)
-    K.layers.Dense(unit=4),
+    K.layers.Dense(units=4),
 ])
 
 batch = np.random.rand(3, 2) # データ数は3
