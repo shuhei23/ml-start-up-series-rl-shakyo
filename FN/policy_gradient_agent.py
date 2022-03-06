e1ba0eb018075af32363aa3d0f572232ca2101e3
shuhei23 <shuhei.toda@gmail.com>
2022-03-06 20:12:39 +0900

    code4-23

--- FN/policy_gradient_agent.py
+++ FN/policy_gradient_agent.py
@@ -1,7 +1,9 @@
+from ast import arg
 import os
 import random
 import argparse
 import numpy as np
+from parso import parse
 from sklearn.preprocessing import StandardScaler
 from sklearn.externals import joblib
 import tensorflow as tf
@@ -184,4 +186,23 @@ class PolicyGradientTrainer(Trainer):
             recent_rewards = self.reward_log[-self.report_interval:]
             self.logger.describe("reward", recent_rewards, episode=episode)
 
-            
+def main(play):
+    env = CartPoleObserver(gym.make("CartPole-v0"))
+    trainer = PolicyGradientTrainer()
+    path = trainer.logger.path_of("policy_gradient_agent.h5")
+    
+    if play:
+        agent = PolicyGradientAgent.load(env, path)
+        agent.play(env)
+    else:
+        trained = trainer.train(env)
+        trainer.logger.plot("Rewards", trainer.reward_log,
+                            trainer.report_interval)
+        trained.save(path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PG Agent")
+    parser.add_argument("--play", action="store_true",
+                        help="play with trained model")
+    args = parser.parse_args()
+    main(args.play)
